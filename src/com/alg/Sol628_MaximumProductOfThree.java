632bb808c6dd2ef1ffd02a6173b98198c478d6f5
Hang <yu59361@gmail.com>
2017-08-08 22:18:48 -0400

    628_MaximumProductOfThree

--- src/com/alg/Sol628_MaximumProductOfThree.java
+++ src/com/alg/Sol628_MaximumProductOfThree.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  */
 public class Sol628_MaximumProductOfThree {
     public static int maximumProduct_sort(int[] nums) {
-        // length >=3
+        // length >= 3
         int n = nums.length;
         Arrays.sort(nums);
         return Math.max(nums[0]*nums[1] * nums[n-1], nums[n-1] * nums[n-2]* nums[n-3]);
