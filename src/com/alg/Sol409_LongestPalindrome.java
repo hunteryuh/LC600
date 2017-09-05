0521f2dd7bf5910041c07df378add87f036911cf
Hang <yu59361@gmail.com>
2017-09-04 22:32:29 -0400

    longest palindrome built from one string

---
+++ src/com/alg/Sol409_LongestPalindrome.java
@@ -0,0 +1,48 @@
+package com.alg;
+
+import java.util.HashSet;
+
+/**
+ * Created by HAU on 9/4/2017.
+ */
+/*
+Given a string which consists of lowercase or uppercase letters,
+        find the length of the longest palindromes that can be built with those letters.*/
+public class Sol409_LongestPalindrome {
+    public static int longestPalindrome(String s){
+        if ( s == null || s.length() == 0) return 0;
+        HashSet<Character> set = new HashSet<>();
+        int count = 0;
+        for (char c: s.toCharArray()){
+            if (set.contains(c)){
+                set.remove(c);
+                count++;
+            }else{
+                set.add(c);
+            }
+        }
+        if (set.isEmpty()) return count*2;
+        return count*2 + 1;
+
+    }
+
+    public static int longestPalindrome2(String s){
+        boolean[] map = new boolean[128];
+        int len = 0;
+        for (char c : s.toCharArray()) {
+            map[c] = !map[c];         // flip on each occurrence, false when seen n*2 times
+            if (!map[c]) len+=2;
+        }
+        if (len < s.length()) len++; // if more than len, atleast one single is present
+        return len;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abccccdd";
+        System.out.println(longestPalindrome(s));
+        System.out.println(longestPalindrome2(s));
+    }
+
+
+}
