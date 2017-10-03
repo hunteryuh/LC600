21ec01886423a73d208960d721ffb57311411451
Hang <yu59361@gmail.com>
2017-10-03 00:13:06 -0400

    reverse linked list from m to n

---
+++ src/com/alg/Sol92_ReverseLInkedListII.java
@@ -0,0 +1,51 @@
+package com.alg;
+
+/**
+ * Created by HAU on 10/2/2017.
+ */
+/*Reverse a linked list from position m to n. Do it in-place and in one-pass.*/
+public class Sol92_ReverseLInkedListII {
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+        }
+    }
+    public static ListNode reverseBetween(ListNode head, int m, int n){
+        if ( m >= n || head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 0; i < m - 1; i++){
+            prev = prev.next;
+        }
+        ListNode start = prev.next; // beginning of the sublist
+        ListNode post = start.next; // to be reversed
+        for (int i = 0; i < n - m; i++){
+            start.next = post.next;
+            post.next = prev.next;
+            prev.next = post;
+            post = start.next;
+        }
+        return dummy.next;
+    }
+    public static void main(String[] args) {
+        ListNode h3 = new ListNode(4);
+        h3.next = new ListNode(5);
+        h3.next.next = new ListNode(1);
+        h3.next.next.next = new ListNode(6);
+        h3.next.next.next.next = new ListNode(2);
+        printList(h3);
+        System.out.println();
+        System.out.println("after reverse");
+        //printList(reverseList(h3));
+        printList(reverseBetween(h3,2,4));
+    }
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+    }
+}
