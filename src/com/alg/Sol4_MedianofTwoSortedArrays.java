d9c69ee0c675f8c9b6691b720250b8ef480734aa
Hang <yu59361@gmail.com>
2017-10-31 23:23:13 -0400

    Median of two sorted arrays--hard-O(log(m,n))-recursively remove half of one array

--- src/com/alg/Sol4_MedianofTwoSortedArrays.java
+++ src/com/alg/Sol4_MedianofTwoSortedArrays.java
@@ -23,7 +23,7 @@ There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
 public class Sol4_MedianofTwoSortedArrays {
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        //double res;
+        //double res; not O(log(min(m,n)) time though
         int m = nums1.length;
         int n = nums2.length;
         int[] newnum = new int[m+n];
@@ -45,5 +45,34 @@ public class Sol4_MedianofTwoSortedArrays {
         int[] num3 = {5};
         System.out.println(findMedianSortedArrays(num1,num2));
         System.out.println(findMedianSortedArrays(num2,num3));
+        System.out.println(findMedianlogn(num1,num3));
     }
+
+    public static double findMedianlogn(int[] a, int[] b){
+        int total = a.length + b.length;
+        if ( total % 2 == 0){
+            return (findkth(total/2, a,b,0,0) + findkth(total/2 + 1, a,b,0,0) )/2.0;
+        }else{
+            return findkth(total/2 + 1, a,b,0,0);
+        }
+    }
+
+    private static double findkth(int k, int[] a, int[] b, int s1, int s2) {
+        if ( s1 >= a.length) return b[s2+k-1];
+        if ( s2 >= b.length) return a[s1+k-1];
+        if ( k == 1){
+            return Math.min(a[s1], b[s2]);
+        }
+        int m1 = s1 + k/2 - 1;
+        int m2 = s2 + k/2 - 1;
+        int mid1 = m1 < a.length ? a[m1] : Integer.MAX_VALUE;
+        int mid2 = m2 < b.length ? b[m2] : Integer.MAX_VALUE;
+        if ( mid1 < mid2){
+            return findkth(k-k/2,a,b, m1 + 1, s2);
+        }else{
+            return findkth(k-k/2, a,b,s1, m2 + 1);
+        }
+
+    }
+
 }
