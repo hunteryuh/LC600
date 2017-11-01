7b3c70c9c9241b66f34c6359aed439aa262ea4ce
Hang <yu59361@gmail.com>
2017-10-31 21:35:03 -0400

    add two numbers; digits are in reverse order

--- src/com/alg/Sol2_addTwoNumbers.java
+++ src/com/alg/Sol2_addTwoNumbers.java
@@ -3,6 +3,10 @@ package com.alg;
 /**
  * Created by HAU on 8/22/2017.
  */
+/*You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.*/
 public class Sol2_addTwoNumbers {
     public static class ListNode{
         int val;
