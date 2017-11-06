8ccde07648e78759645167669a8f5ad0abd27820
Hang <yu59361@gmail.com>
2017-11-05 23:09:47 -0500

    level order traversal, dfs

---
+++ src/com/alg/Sol102_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,69 @@
+package com.alg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HAU on 11/5/2017.
+ */
+/*Given a binary tree, return the level order traversal of its nodes' values.
+(ie, from left to right, level by level).*/
+/*Given binary tree [3,9,20,null,null,15,7],
+*
+*
+* [
+  [3],
+  [9,20],
+  [15,7]
+]*/
+public class Sol102_BinaryTreeLevelOrderTraversal {
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            val = x;
+        }
+    }
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        // using dfs
+        List<List<Integer>> res = new ArrayList<>();
+        levelhelper(root, res, 0);
+        return res;
+    }
+
+    private static void levelhelper(TreeNode root, List<List<Integer>> res, int depth) {
+        if ( root == null) return ;
+        if ( depth == res.size()){
+            res.add(new ArrayList<>());
+        }
+        res.get(depth).add(root.val);
+        levelhelper(root.left, res,depth + 1);
+        levelhelper(root.right, res, depth + 1);
+    }
+
+    public static void main(String[] args) {
+        //      1
+        //    /   \
+        //   2     3
+        //  / \   /
+        // 4   5 6
+        //    /
+        //   7
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node5.left = node7;
+        System.out.println(levelOrder(node1));
+    }
+}
