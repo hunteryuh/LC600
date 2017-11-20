e053e0348d43b6e0c10cde8cb3dd8a0253c3a870
Hang <yu59361@gmail.com>
2017-11-19 23:12:28 -0500

    DFS backtracking; candidates DONOT have duplicates; each candidate can be used multiple times in te solution set

--- src/com/alg/Sol39_CombinationSum.java
+++ src/com/alg/Sol39_CombinationSum.java
@@ -18,7 +18,12 @@ a target number (T), find all unique combinations in C where the candidate numbe
         Note:
         All numbers (including target) will be positive integers.
         The solution set must not contain duplicate combinations.
-        For example, given candidate set [2, 3, 6, 7] and target 7,*/
+        For example, given candidate set [2, 3, 6, 7] and target 7,
+        A solution set is:
+[
+  [7],
+  [2, 2, 3]
+]*/
 public class Sol39_CombinationSum {
     public static List<List<Integer>> combinationSum(int[] candidates, int target){
         ArrayList<List<Integer>> result = new ArrayList<>();
@@ -38,7 +43,8 @@ public class Sol39_CombinationSum {
         for ( int i = index; i < candidates.length; i++){
             if (candidates[i] > target) return;
             subset.add(candidates[i]);
-            helper(candidates,i,subset,target - candidates[i],result);
+            //The same repeated number may be chosen from C unlimited number of times.
+            helper(candidates,i,subset,target - candidates[i],result); // still start from i, not i + 1
             subset.remove(subset.size() - 1);
         }
 
