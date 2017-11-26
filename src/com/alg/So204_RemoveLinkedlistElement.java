157f5efbe5c8e533394f508f71cb27744bea26ec
Hang <yu59361@gmail.com>
2017-11-25 22:40:50 -0500

    remove linkedlist element with vertain val, need clean!

---
+++ src/com/alg/So204_RemoveLinkedlistElement.java
@@ -0,0 +1,38 @@
+package com.alg;
+
+/**
+ * Created by HAU on 11/25/2017.
+ */
+/*Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5*/
+public class So204_RemoveLinkedlistElement {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    public static ListNode removeElements(ListNode head, int val) {
+        if ( head == null) return null;
+        ListNode cur = head;
+        while (cur.next != null){
+            if(cur.next.val == val){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return head.val == val? head.next: head;
+    }
+
+    public static ListNode removeEle(ListNode head, int val){
+        if ( head == null) return null;
+        head.next = removeEle(head.next,val);
+        return head.val == val? head.next : head;
+    }
+}
