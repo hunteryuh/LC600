98f6484fd69811d5d8148388d80863c0a7541052
Hang <yu59361@gmail.com>
2017-11-27 23:52:47 -0500

    buysell stock 2, multiple transactions, one way pass

--- src/com/alg/Sol122BestTimetoBuyandSellStock_II.java
+++ src/com/alg/Sol122BestTimetoBuyandSellStock_II.java
@@ -8,6 +8,7 @@ package com.alg;
 //(ie, buy one and sell one share of the stock multiple times).
 
 public class Sol122BestTimetoBuyandSellStock_II {
+    //(Simple One Pass) O(n) time O(1) space
     public static int maxProfit(int[] prices) {
         if (prices.length==0) return 0;
         int profit = 0;
@@ -20,6 +21,25 @@ public class Sol122BestTimetoBuyandSellStock_II {
         return profit;
 
     }
+    //(Peak Valley Approach)
+    public static int maxP2(int[] prices){
+        if (prices.length==0) return 0;
+        int valley = prices[0], peak = prices[0];
+        int res = 0, i = 0;
+        int n = prices.length;
+        while ( i < n - 1){
+            while(i < n - 1 && prices[i] >= prices[i+1]){
+                i++;
+            }
+            valley = prices[i];
+            while ( i < n - 1 && prices[i] <= prices[i+1]){
+                i++;
+            }
+            peak = prices[i];
+            res += peak - valley;
+        }
+        return res;
+    }
 
 
 
@@ -29,6 +49,7 @@ public class Sol122BestTimetoBuyandSellStock_II {
         //int sol = 0;
         System.out.println(maxProfit(prices1));
         System.out.println(maxProfit(prices2));
+        System.out.println(maxP2(prices1));
         //System.out.println(maxP2(prices1));
         //System.out.println(maxP2(prices2));
 
