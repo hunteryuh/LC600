044f651dfb3a28577a63b23c8c7c7b87cfc0ccec
Hang <yu59361@gmail.com>
2017-11-28 21:37:08 -0500

    implement queue using 2 stacks.

---
+++ src/com/alg/Sol232_ImplementQueueUsingStacks.java
@@ -0,0 +1,47 @@
+package com.alg;
+
+import java.util.Stack;
+
+/**
+ * Created by HAU on 11/28/2017.
+ */
+/*Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.*/
+public class Sol232_ImplementQueueUsingStacks {
+    class MyQueue {
+        private Stack<Integer> stack = new Stack<>();
+        // push - O(n)， pop - O(1)， peek - O(1)， isEmpty - O(1).  Space Complexity - O(n)
+        /** Initialize your data structure here. */
+        public MyQueue() {
+
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            Stack<Integer> tmp = new Stack<>();
+            while (!stack.isEmpty()) tmp.push(stack.pop());
+            stack.push(x);
+            while(!tmp.isEmpty()) stack.push(tmp.pop());
+            
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            return stack.pop();
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            return stack.peek();
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return stack.isEmpty();
+        }
+    }
+}
