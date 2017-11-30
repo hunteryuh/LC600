16b52a039fc10bbd84f5ac55ca2bb8ee4bd46a5a
Hang <yu59361@gmail.com>
2017-11-29 23:08:38 -0500

    remove dup in sorted array; two pointer, slow and fast. time O(n)

--- src/com/alg/Sol26_RemoveDuplicatesFromSortedArray.java
+++ src/com/alg/Sol26_RemoveDuplicatesFromSortedArray.java
@@ -40,4 +40,16 @@ public class Sol26_RemoveDuplicatesFromSortedArray {
         int[] n2 = {1};
         System.out.println(removeDuplicates(n2));
     }
+    // two pointer way
+    public static int removeDup(int[] nums){
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1;j < nums.length; j++){
+            if( nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
 }
