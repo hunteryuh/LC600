2924e70380f4fb91cd1f3b76418c5aa2437cba89
Hang <yu59361@gmail.com>
2017-12-02 16:02:27 -0500

    lowest common ancestor of a binary tree, add test cases; recursive call, what to return.

--- src/com/alg/Sol236_LowestCommonAncestorofABinaryTree.java
+++ src/com/alg/Sol236_LowestCommonAncestorofABinaryTree.java
@@ -25,4 +25,29 @@ public class Sol236_LowestCommonAncestorofABinaryTree {
         }
         return left != null? left: right;
     }
+    public static void main(String[] args) {
+        //      8
+        //    /   \
+        //   2     10
+        //  / \   /
+        // 1   5 9
+        //    /
+        //   3
+        TreeNode node1 = new TreeNode(8);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(10);
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(9);
+        TreeNode node7 = new TreeNode(3);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node5.left = node7;
+        System.out.println(lowestCommonAncestor(node1, node3, node7).val); //8
+        System.out.println(lowestCommonAncestor(node1, node2, node7).val); //2
+    }
 }
