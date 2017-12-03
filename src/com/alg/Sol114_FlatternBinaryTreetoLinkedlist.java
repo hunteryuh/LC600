6f1e16e42df1f8123fa4057e6b515b72f9093adb
Hang <yu59361@gmail.com>
2017-12-03 13:53:29 -0500

    flattern binary tree to a regular linkedlist- recursive: right first then left; update root.right

--- src/com/alg/Sol114_FlatternBinaryTreetoLinkedlist.java
+++ src/com/alg/Sol114_FlatternBinaryTreetoLinkedlist.java
@@ -3,7 +3,31 @@ package com.alg;
 /**
  * Created by HAU on 11/25/2017.
  */
-/*Given a binary tree, flatten it to a linked list in-place.*/
+/*Given a binary tree, flatten it to a linked list in-place.
+* For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+click to show hints.
+
+Hints:
+If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.*/
 public class Sol114_FlatternBinaryTreetoLinkedlist {
     public static class TreeNode{
         int val;
