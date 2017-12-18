7cbbc8c001fa2abad024390b59a4e333696f13b3
Hang <yu59361@gmail.com>
2017-12-17 22:45:47 -0500

    unique word abbr, hashmap; <2 string or >2

--- src/com/alg/Sol288_UniqueWordAbbreviation.java
+++ src/com/alg/Sol288_UniqueWordAbbreviation.java
@@ -5,7 +5,21 @@ import java.util.HashMap;
 /**
  * Created by HAU on 6/26/2017.
  */
+/*An abbreviation of a word follows the form <first letter><number><last letter>. Below are some examples of word abbreviations:
 
+a) it                      --> it    (no abbreviation)
+
+     1
+b) d|o|g                   --> d1g
+
+              1    1  1
+     1---5----0----5--8
+c) i|nternationalizatio|n  --> i18n
+
+              1
+     1---5----0
+d) l|ocalizatio|n          --> l10n
+*/
 /*Assume you have a dictionary and given a word, find whether
 its abbreviation is unique in the dictionary. A word's abbreviation is unique
 if no other word from the dictionary has the same abbreviation.
@@ -27,13 +41,13 @@ public class Sol288_UniqueWordAbbreviation {
             for (String str:dictionary) {
                 String key;
                 if (str.length() <= 2) {
-                    key = str.substring(0);
+                    key = str;
                 } else {
                     key = str.charAt(0) + Integer.toString(str.length() - 2) + str.substring(str.length() - 1);
                 }
                 if(map.containsKey(key)) {
                     if (map.get(key).equals(str)){
-                        map.put(key,"");
+                        map.put(key,""); // if more than one string maps to the same key, then this key will be invalid, set to ""
                     }
                 }
                 else map.put(key,str);
@@ -51,10 +65,10 @@ public class Sol288_UniqueWordAbbreviation {
         public static void main(String[] args) {
             String[] dict = {"door","deer","sake","cane","it","it"};
             ValidWordAbbr dic = new ValidWordAbbr(dict);
-            System.out.println(dic.isUnique("clan"));
-            System.out.println(dic.isUnique("safe"));
-            System.out.println(dic.isUnique("it"));
-            System.out.println(dic.isUnique("daar"));
+            System.out.println(dic.isUnique("clan")); // true
+            System.out.println(dic.isUnique("safe")); // false
+            System.out.println(dic.isUnique("it")); //false
+            System.out.println(dic.isUnique("daar")); // false
         }
     }
 }
