7eceb7e9c4b1960055a17b4e192e8b12b6312c1a
Hang <yu59361@gmail.com>
2017-12-25 22:55:37 -0500

    right rotate,

---
+++ src/com/alg/Sol_0amz_rightRotate.java
@@ -0,0 +1,24 @@
+package com.alg;
+
+/**
+ * Created by HAU on 12/25/2017.
+ */
+/* check if string 1 can be right rotated by s2
+ * ir yes, return how many chars to rotate */
+public class Sol_0amz_rightRotate {
+    public static int rightRot(String s1, String s2){
+        if (s1 == null || s2 == null || s1.length() == 0 || s2.length() == 0 ||
+                s1.length() != s2.length()){
+            return -1;
+        }
+        String s = s1 + s1;
+        return s.indexOf(s2)!= -1 ? s.indexOf(s2): -1;
+
+    }
+
+    public static void main(String[] args) {
+        String s1 = "amazon";
+        String s2 = "azonam";
+        System.out.println(rightRot(s1,s2));
+    }
+}
