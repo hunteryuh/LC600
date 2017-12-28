43eb48bcc64741327ffc0193c123232d9d5e5405
Hang <yu59361@gmail.com>
2017-12-28 14:56:17 -0500

    check if s2 can right rotate to s1 (string)

--- src/com/alg/Sol0_amz_rightRotate.java
+++ src/com/alg/Sol0_amz_rightRotate.java
@@ -5,14 +5,14 @@ package com.alg;
  */
 /* check if string 1 can be right rotated by s2
  * ir yes, return how many chars to rotate */
-public class Sol_0amz_rightRotate {
+public class Sol0_amz_rightRotate {
     public static int rightRot(String s1, String s2){
         if (s1 == null || s2 == null || s1.length() == 0 || s2.length() == 0 ||
                 s1.length() != s2.length()){
             return -1;
         }
         String s = s1 + s1;
-        return s.indexOf(s2)!= -1 ? s.indexOf(s2): -1;
+        return s.indexOf(s2)!= -1 ? s.indexOf(s2): -1; // ? 1: -1  1 means it can be right rotated.
 
     }
 
