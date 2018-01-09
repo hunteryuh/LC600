5a7cae7b970163b00ee3bc7c923463c9c11e4f47
Hang <yu59361@gmail.com>
2018-01-09 07:14:03 -0500

    amazon, window sum of size k ( k < nums.length)  ( int[] and list<Integer>)

--- src/com/alg/Sol0_amz_windowSum.java
+++ src/com/alg/Sol0_amz_windowSum.java
@@ -1,5 +1,9 @@
 package com.alg;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by HAU on 12/27/2017.
  */
@@ -18,5 +22,31 @@ public class Sol0_amz_windowSum {
             res[i] = res[i - 1] - nums[i-1]+nums[i-1+k];
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,7,8,5};
+        int[] res = windowSum(nums,3);
+        System.out.println(Arrays.toString(res));
+        Integer[] n = {1,2,7,8,4};
+        List<Integer> list = new ArrayList<>(Arrays.asList(n));
+        System.out.println(windowSumList(list,3));
+    }
+    // list as input
+    public static List<Integer> windowSumList(List<Integer> nums, int k){
+        List<Integer> ans = new ArrayList<>();
+        if(nums == null || nums.size() == 0 || k <=0) return ans;
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            count++;
+            if(count >= k){
+                int sum = 0;
+                for(int j = i; j>= i - k + 1; j--){
+                    sum += nums.get(j);
+                }
+                ans.add(sum);
+            }
+        }
+        return ans;
     }
 }
