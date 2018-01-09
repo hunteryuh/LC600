72291923b8260a9b0b5bda9c5c8be4f1b61ac3e8
Hang <yu59361@gmail.com>
2018-01-09 05:30:53 -0500

    valid palindrome ii, two pointer

---
+++ src/com/alg/Sol680_ValidPalindromeII.java
@@ -0,0 +1,47 @@
+package com.alg;
+
+/**
+ * Created by HAU on 1/9/2018.
+ */
+/*Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+Note:
+The string will only contain lowercase characters a-z. The maximum length of the string is 50000.*/
+public class Sol680_ValidPalindromeII {
+    public static boolean validPalindromeII(String s) {
+        if (s == null || s.length() == 0) return true;
+        int n = s.length();
+        int i = 0, j = n - 1;
+        while ( i < j ){
+            if(s.charAt(i) != s.charAt(j)){
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+
+
+    }
+    private static boolean isPalindrome(String s, int i, int j ){
+        while( i<j && i >= 0 && j <= s.length() - 1){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "aba";
+        String s2 = "abca";
+        System.out.println(validPalindromeII(s1));
+        System.out.println(validPalindromeII(s2));
+    }
+}
