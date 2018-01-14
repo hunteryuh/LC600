76eb787568ff2a7de8d81ba0647e0f3f294e3708
Hang <yu59361@gmail.com>
2018-01-14 00:56:31 -0500

    remove Vowels in a string, replaceAll, indexOf(ch) contains(ch + '"")

--- src/com/alg/Sol0_amz_removeVowel.java
+++ src/com/alg/Sol0_amz_removeVowel.java
@@ -13,9 +13,34 @@ public class Sol0_amz_removeVowel {
         }
         return sb.toString();
     }
+    public static String removeVowel3(String str){
+        String vowels = "aeiouAEIOU";
+        String res = str.replaceAll("[aeiouAEIOU]","");
+        return res;
+    }
 
     public static void main(String[] args) {
         String s = "leetcode";
         System.out.println(removeVowel(s));
+        System.out.println(removeV2(s));
+        System.out.println(removeVowel3(s));
     }
+    // method2
+    public static String removeV2(String str) {
+        // corner case
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        String vowels = "AEIOUaeiou";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (vowels.contains(ch + "")) continue;
+            sb.append(ch);
+        }
+        return sb.toString();
+
+    }
+
+
 }
