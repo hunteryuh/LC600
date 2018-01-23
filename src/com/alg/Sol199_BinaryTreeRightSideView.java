cda849a2a4f80811d73280ba43475e063a11736e
Hang <yu59361@gmail.com>
2018-01-23 04:28:31 -0500

    two stacks, saving node and depth,

--- src/com/alg/Sol199_BinaryTreeRightSideView.java
+++ src/com/alg/Sol199_BinaryTreeRightSideView.java
@@ -12,8 +12,10 @@ Given the following binary tree,
    1            <---
  /   \
 2     3         <---
- \     \
-  5     4       <---
+ \
+  5              <---
+ /
+4                <---
 You should return [1, 3, 4].*/
 public class Sol199_BinaryTreeRightSideView {
     public static class TreeNode{
@@ -52,5 +54,19 @@ public class Sol199_BinaryTreeRightSideView {
             rightView.add(rightMostValueAtDepth.get(depth));
         }
         return rightView;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(5);
+        TreeNode n5 = new TreeNode(6);
+        n1.left = n2;
+        n1.right = n3;
+        n2.right = n4;
+        n4.left = n5;
+        System.out.println(rightSideView(n1));   // [1,3,5,6]
+
     }
 }
