5b3847c94fbac4f92c82baebe95a58391180ad9d
Hang <yu59361@gmail.com>
2018-01-24 08:43:07 -0500

    group anagram, using sort

--- src/com/alg/Sol49_GroupAnagram.java
+++ src/com/alg/Sol49_GroupAnagram.java
@@ -9,8 +9,9 @@ import java.util.*;
 
 For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], */
 public class Sol49_GroupAnagram {
+    //time : Nklog(k)   N is length of strs, k is the max length of string in strs
     public static List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 0) return new ArrayList<>();
+        if (strs.length == 0) return new ArrayList<List<String>>();
         Map<String, List> res = new HashMap<>();
         for (String s : strs){
             char[] car = s.toCharArray();
@@ -29,5 +30,29 @@ public class Sol49_GroupAnagram {
     public static void main(String[] args) {
         String[] strs ={"eat", "tea", "tan", "ate", "nat", "bat"};
         System.out.println(groupAnagrams(strs));
+        System.out.println(groupAna(strs));
+    }
+    // method 2, count array
+    public static List<List<String>> groupAna(String[] strs){
+        if ( strs.length == 0 ) return  new ArrayList<List<String>>();
+        Map<String, List> ans = new HashMap<>();
+        int[] count = new int[26];
+        for(String s: strs){
+            Arrays.fill(count,0);
+            for(char c: s.toCharArray()){
+                count[c-'a'] ++;
+            }
+            StringBuilder sb = new StringBuilder("");
+            for(int i = 0; i < 26; i++){
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if(!ans.containsKey(key)){
+                ans.put(key, new ArrayList());
+            }
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
     }
 }
