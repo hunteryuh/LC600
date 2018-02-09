f29fca7db9dccddfa16d5943d3c0c7abf51fcbfe
Hang <yu59361@gmail.com>
2018-02-09 14:50:10 -0500

    layer by layer while and 4 for

---
+++ src/com/alg/Sol59_SpiralMatrixII.java
@@ -0,0 +1,54 @@
+package com.alg;
+
+import java.util.Arrays;
+
+/**
+ * Created by HAU on 2/9/2018.
+ */
+/*Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+For example,
+Given n = 3,
+
+You should return the following matrix:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]*/
+public class Sol59_SpiralMatrixII {
+    public static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        if ( n <= 0) return matrix;
+        int num = 1;
+        int rowbegin = 0;
+        int rowend = n - 1;
+        int colbegin = 0;
+        int colend = n - 1;
+        while( rowbegin <= rowend && colbegin <= colend){
+            for(int i = colbegin; i <= colend; i++){
+                matrix[rowbegin][i] = num++;
+            }
+            rowbegin++;
+            for(int i = rowbegin; i<=rowend; i++){
+                matrix[i][colend] = num++;
+            }
+            colend--;
+            for(int i = colend; i >= colbegin; i--){
+                matrix[rowend][i] = num++;
+            }
+            rowend--;
+            for(int i = rowend; i>= rowbegin; i--){
+                matrix[i][colbegin] = num++;
+            }
+            colbegin++;
+        }
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] m = generateMatrix(n);
+        System.out.println(Arrays.deepToString(m));
+    }
+}
