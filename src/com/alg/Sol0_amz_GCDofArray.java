74ff26c9d85c86dce01067ee28f42a6e3c42d0b6
hunteryuh <yu59361@gmail.com>
2018-02-23 21:09:48 -0500

    gcd

--- src/com/alg/Sol0_amz_GCDofArray.java
+++ src/com/alg/Sol0_amz_GCDofArray.java
@@ -18,20 +18,28 @@ public class Sol0_amz_GCDofArray {
     private static int getGCD(int n1, int n2) {
         if ( n1 == 0 || n2 == 0) return 0;
         while(n1 != 0 && n2 != 0){
-            if ( n2 > n1){
+/*            if ( n2 > n1){
                 n1 = n1 ^ n2;
                 n2 = n1 ^ n2;
-                n1 = n1 ^ n2; // switch n2 and n2
-            }
+                n1 = n1 ^ n2; // switch n1 and n2
+            }*/
             int temp = n1 % n2;
             n1 = n2;
             n2 = temp;
         }
         return n1 + n2;
     }
+    private static int findGCD(int a, int b){
+        while( b!= 0){
+            int temp = a;
+            a = b;
+            b = temp % a;
+        }
+        return a; //( if b == 0) return a
+    }
 
     public static void main(String[] args) {
-        int[] nums = { 3,6,90};
+        int[] nums = { 6,90,27};
         System.out.println(GCDArray(nums));
     }
 }
