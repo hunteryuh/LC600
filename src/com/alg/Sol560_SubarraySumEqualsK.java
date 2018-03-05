ab3a05054df0685e3d7ee5740d3fd3c9c9e64ceb
hunteryuh <yu59361@gmail.com>
2018-03-05 16:19:05 -0500

    hashmap, one traverse O(n)

--- src/com/alg/Sol560_SubarraySumEqualsK.java
+++ src/com/alg/Sol560_SubarraySumEqualsK.java
@@ -1,5 +1,8 @@
 package com.alg;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by HAU on 5/24/2017.
  */
@@ -7,7 +10,7 @@ package com.alg;
 Given an array of integers and an integer k,
 you need to find the total number of continuous subarrays whose sum equals to k.
 */
-public class Sol560SubarraySumEqualsK {
+public class Sol560_SubarraySumEqualsK {
     public static int subarraySum(int[] nums, int k){
         if (nums.length ==0) return 0;
         int sum = 0;
@@ -31,5 +34,24 @@ public class Sol560SubarraySumEqualsK {
         int[] nums = {1,1,1};
         int k = 2;
         System.out.println(subarraySum(nums,k));
+    }
+    // hashmap, time O(n)
+    public static int subArraySum(int[] nums, int k){
+        Map<Integer,Integer> map = new HashMap<>();
+        // key: sum;  value: occurence of the sum
+        map.put(0,1);
+        int sum = 0;
+        int count = 0;
+        for( int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            count += map.getOrDefault(sum - k, 0);
+            //if ( map.containsKey(sum - k)
+            //{
+            //  count += map.get(sum - k);
+            //}
+            //
+            map.put(sum, map.getOrDefault(sum,0) + 1);
+        }
+        return count;
     }
 }
