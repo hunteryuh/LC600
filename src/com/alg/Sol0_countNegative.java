d9720722b1acc8cbed7dcd15877afdffc4be214a
hunteryuh <yu59361@gmail.com>
2018-03-14 00:02:53 -0400

    O(m+n) with while loop, rows and columns are sorted; for loop also works with a slight change on the counter

--- src/com/alg/Sol0_countNegative.java
+++ src/com/alg/Sol0_countNegative.java
@@ -6,18 +6,18 @@ package com.alg;
 /*count number of negative numbers in a matrix, both row and columns are sorted in
 * ascending order*/
 public class Sol0_countNegative {
-    public static int countNegative(int[][] matrix){
+    public static int countNegative(int[][] matrix) {
         // time complexity is O(N + M)
         int count = 0;
         int i = 0;
         int m = matrix.length;
         int n = matrix[0].length;
         int j = n - 1;
-        while ( i <= m - 1 && j >=0){
-            if(matrix[i][j] < 0){
+        while (i <= m - 1 && j >= 0) {
+            if (matrix[i][j] < 0) {
                 count += j + 1;
                 i++;
-            }else{
+            } else {
                 j--;
             }
         }
@@ -26,10 +26,31 @@ public class Sol0_countNegative {
 
     public static void main(String[] args) {
         int[][] m = {
-                { -4, -2, -1, 4, 7 },
-                { -3, -1, 0, 4, 8 },
-                { -1, 3, 5, 7, 9 }
+                {-4, -2, -1, 4, 7},
+                {-3, -1, 0, 4, 8},
+                {-1, 3, 5, 7, 9}
         };
         System.out.println(countNegative(m));
+        System.out.println(countNegativefor(m));
+
+    }
+
+    public static int countNegativefor(int[][] matrix) {
+        int count = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int j = n - 1; j >= 0; j--) {
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][j] >= 0) {
+                    break;
+                } else {
+                    //count += j + 1;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
     }
 }
