e641b6b99f0e13a6d47a11630d9bc066a21d4a05
hunteryuh <yu59361@gmail.com>
2018-03-16 00:06:27 -0400

    sum of subtree, recursion and comparison

--- src/com/alg/Sol0_amz_MaximumSubtree.java
+++ src/com/alg/Sol0_amz_MaximumSubtree.java
@@ -45,10 +45,12 @@ public class Sol0_amz_MaximumSubtree {
         TreeNode n6 = new TreeNode(-4);
         TreeNode n7 = new TreeNode(-5);
         n1.left = n2;
+        n1.right = n3;
         n2.left = n4;
         n2.right = n5;
         n3.left = n6;
         n3.right = n7;
         System.out.println(findMaxSubtree(n1).val);
     }
+
 }
