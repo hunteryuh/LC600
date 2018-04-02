eccb417c97f29af1864affade6e07de649e241d4
hunteryuh <yu59361@gmail.com>
2018-04-01 20:59:10 -0400

    from top-right corner, time O(m+n)

--- src/com/alg/Sol74_SearchA2DMatrix.java
+++ src/com/alg/Sol74_SearchA2DMatrix.java
@@ -42,5 +42,24 @@ public class Sol74_SearchA2DMatrix {
                 {23, 30, 34, 50}
         };
         System.out.println(searchMatrix(matrix,3));
+        System.out.println(searchMatrix2(matrix,3));
+    }
+    public static boolean searchMatrix2(int[][] matrix, int target) {
+        if( matrix == null || matrix.length ==0 )
+            return false;
+        int start = 0, rows = matrix.length, cols = matrix[0].length;
+        int end = rows*cols - 1;
+        while ( start <= end){
+            int mid = (start + end )/2;
+            if ( matrix[mid/cols][mid%cols] == target){
+                return true;
+            }
+            if( matrix[mid/cols][mid%cols] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return false;
     }
 }
