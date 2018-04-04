8ece5f679db5e1e9085eefb374df14f0f36937f2
hunteryuh <yu59361@gmail.com>
2018-04-03 22:37:16 -0400

    duplicated elements in the array; check if used in the list

--- src/com/alg/Sol47_PermutionsII.java
+++ src/com/alg/Sol47_PermutionsII.java
@@ -27,7 +27,12 @@ public class Sol47_PermutionsII {
         dfs(nums,res,sol,used);
         return res;
     }
+/*求全排列，但元素可能有重复。去重复就成为了关键。今天好好思考了一下dfs+回溯，比如1134，
+最外层就是求出第一个元素，比如 1， 2， 3， 里面的嵌套dfs再负责第二，三，四个元素。
+去重复的方法是传递一个visited数组，把排序后相同的元素看成一个cluster，假如nums[i] == nums[i - 1]，
+但i-1没有被访问过，说明整个cluster不被访问，跳过整个cluster。
 
+Time complexity - O(n!), Space Complexity - O(n)。*/
     private static void dfs(int[] nums, List<List<Integer>> res, List<Integer> sol, boolean[] used) {
         if(sol.size() == nums.length){
             res.add(new ArrayList<>(sol));
@@ -36,7 +41,12 @@ public class Sol47_PermutionsII {
         for (int i = 0; i < nums.length ;i++){
             if(used[i]) continue;
             if(i>0 && nums[i] == nums[i-1] && !used[i-1]) continue;
-            //when a number has the same value with its previous, we can use this number only if his previous is used
+            //when a number has the same value with its previous,
+            // we can use this number only if his previous is used
+            /*比如，给出一个排好序的数组，[1,2,2]，那么第一个2和第二2如果在结果中互换位置，
+            我们也认为是同一种方案，所以我们强制要求相同的数字，原来排在前面的，在结果
+            当中也应该排在前面，这样就保证了唯一性。所以当前面的2还没有使用的时候，就
+            不应该让后面的2使用。*/
             used[i] = true;
             sol.add(nums[i]);
             dfs(nums,res,sol,used);
