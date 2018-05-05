6f7714feca2f71970be867f4e1d634394965d0e1
hunteryuh <yu59361@gmail.com>
2018-05-05 14:37:29 -0700

    add a int[] method; count 0,1,2 cases

--- src/com/alg/Sol788_RotatedDigits.java
+++ src/com/alg/Sol788_RotatedDigits.java
@@ -50,6 +50,42 @@ public class Sol788_RotatedDigits {
     public static void main(String[] args) {
         int n = 10;
         System.out.println(rotatedDigits(n)); //4
+        int m = 19;
+        System.out.println(rotatedDigits2(m)); //8
+    }
+    public static int rotatedDigits2(int N) {
+    /*Using a int[] for dp.
+dp[i] = 0, invalid number
+dp[i] = 1, valid and same number
+dp[i] = 2, valid and different number*/
+        int[] dp = new int[N+1];
+        int count = 0;
+        for (int i = 0; i <= N; i++){
+            if(i<10){
+                if (i == 0 || i == 1 || i == 8){
+                    dp[i] = 1;
+                }else if( i ==2 || i == 5 || i == 6 || i ==9){
+                    dp[i] = 2;
+                    count++;
+                }
+            }else{
+                int a = dp[i/10];
+                int b = dp[i%10];
+                if ( a ==1 && b ==1){
+                    dp[i] = 1;
+                }else if( a >= 1 && b >= 1){
+                    dp[i] = 2;
+                    count++;
+                }
+            }
+        }
+        return  count;
+        /*Basics:
+        0=<i<10
+
+    dp[i] = 1   dp[i/10] ==1 && dp[i%10]==1
+           2  dp[i/10] ==1 && dp[i%10]==2 ||  dp[i/10] ==2 && dp[i%10]==1
+           0 other*/
     }
 
 }
