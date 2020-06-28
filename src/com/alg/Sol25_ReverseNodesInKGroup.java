38b43c8fb7b072f5fda33a969afd173cf166477a
hunteryuh <yu59361@gmail.com>
2020-06-28 12:12:02 -0700

    Add reveserNodesInKGroup method 2

--- src/com/alg/Sol25_ReverseNodesInKGroup.java
+++ src/com/alg/Sol25_ReverseNodesInKGroup.java
@@ -18,6 +18,7 @@ For k = 2, you should return: 2->1->4->3->5
 
 For k = 3, you should return: 3->2->1->4->5*/
 public class Sol25_ReverseNodesInKGroup {
+
     public static class ListNode {
         int val;
         ListNode next;
@@ -26,7 +27,9 @@ public class Sol25_ReverseNodesInKGroup {
             val = x;
         }
     }
-    public static ListNode reverseKGroup(ListNode head, int k) {
+
+    // method 1
+    public static ListNode reverseKGroup1(ListNode head, int k) {
         ListNode cur = head;
         int count = 0;
         while(cur!=null && count!=k){
@@ -34,7 +37,7 @@ public class Sol25_ReverseNodesInKGroup {
             count++;
         }
         if ( count == k){
-            cur = reverseKGroup(cur,k); // reverse list whose head is ((k+1)th node
+            cur = reverseKGroup1(cur,k); // reverse list whose head is ((k+1)th node
             while(count >0){
                 ListNode tmp = head.next;
                 head.next = cur;
@@ -47,6 +50,48 @@ public class Sol25_ReverseNodesInKGroup {
         return head;
 
     }
+
+    // method 2
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode node = dummy;
+        while (node != null) {
+            node = reverseNextK(node, k);
+        }
+        return dummy.next;
+    }
+
+    // n0 -> n1 -> n2 ... -> nk -> nkplus
+    // n0 -> nk -> nk-1 ...-> n1 -> nkplus
+
+    private static ListNode reverseNextK(ListNode node0, int k) {
+        // are there k or more nodes to reverse
+        ListNode nk = node0;
+        for (int i = 0; i < k; i++) {
+            nk = nk.next;
+            if (nk == null) return null;
+        }
+        // now nk = nk;
+        // reverse list from n1 to nk
+        ListNode nkplus = nk.next;
+        ListNode n1 = node0.next;
+        ListNode pre = null;
+        ListNode curr = node0.next;
+        for (int i = 0; i < k; i++) {
+            ListNode temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+        }
+
+        node0.next = pre; // node0.next = nk;
+        n1.next = curr;  // n1.next = nkplus;
+        return n1;
+
+    }
+
     public static void main(String[] args) {
         ListNode h3 = new ListNode(1);
         h3.next = new ListNode(2);
@@ -56,8 +101,9 @@ public class Sol25_ReverseNodesInKGroup {
         printList(h3);
         System.out.println();
         System.out.println("after Swap k groups");
-        printList(reverseKGroup(h3,3));
+        printList(reverseKGroup(h3,2));
     }
+
     public static void printList(ListNode node) {
         while (node != null) {
             System.out.print(node.val + "->");
